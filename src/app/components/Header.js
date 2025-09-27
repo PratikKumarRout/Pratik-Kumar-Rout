59be5f5a0dca91c308fb03662c5b01deca4fc775
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-09-27 10:56:11 +0530

    Intial design to git

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,21 +3,21 @@ import PlaceholderCircle from "./PlaceholderCircle";
 
 export default function Header() {
     return (
-            <header>
-                <div style={{
-                    display: "flex",
-                    alignItems: "center"
-                }}>
-                    <PlaceholderCircle size="2.5rem" />
-                    <PlaceholderBox width="300px" height="30px" shade="#666" /> </div>
-                <div style={{
-                    display: "flex", gap: "1rem"
-                }}>
-                    <PlaceholderBox width="60px" height="20px" shade="#888" />
-                    <PlaceholderBox width="60px" height="20px" shade="#888" />
-                    <PlaceholderBox width="60px" height="20px" shade="#888" />
-                    <PlaceholderBox width="60px" height="20px" shade="#888" />
-                </div>
-            </header>
+        <header>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+            }}>
+                <PlaceholderCircle size="2.5rem" shade="#fff" />
+                <PlaceholderBox width="300px" height="30px" shade="#fff" /> </div>
+            <div style={{
+                display: "flex", gap: "1rem", paddingRight: "10px", alignItems: "center", color: "#fff"
+            }}>
+                <PlaceholderBox width="60px" height="20px" shade="#fff" />
+                <PlaceholderBox width="60px" height="20px" shade="#fff" />
+                <PlaceholderBox width="60px" height="20px" shade="#fff" />
+                <PlaceholderBox width="60px" height="20px" shade="#fff" />
+            </div>
+        </header>
     );
 }
--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -4,33 +4,53 @@ import PlaceholderCircle from "./PlaceholderCircle";
 
 export default function Hero() {
   return (
-    <section className="hero">
-      {/* Intro placeholders */}
-      <div>
-        <PlaceholderBox width="300px" height="40px" shade="#333" />
-        <PlaceholderBox width="200px" height="20px" shade="#666" />
-        <PlaceholderBox width="150px" height="20px" shade="#999" />
-      </div>
+    <section style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "space-around"
+    }}>
 
-      {/* Achievements row */}
-      <div className="hero-achievements">
-        <PlaceholderBox width="200px" height="30px" shade="#444" />
-        <PlaceholderBox width="200px" height="30px" shade="#666" />
-        <PlaceholderBox width="200px" height="30px" shade="#888" />
-      </div>
+      <div style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}>
+        {/* Intro placeholders */}
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-around"}}>
+          <div>
+            <div>
+              <PlaceholderBox width="700px" height="70px" shade="#333" />
+              <PlaceholderBox width="300px" height="50px" shade="#666" />
+              <PlaceholderBox width="200px" height="40px" shade="#999" />
+            </div>
 
+            {/* Achievements row */}
+            <div className="hero-achievements">
+              <PlaceholderBox width="200px" height="200px" shade="#444" />
+              <PlaceholderBox width="200px" height="200px" shade="#666" />
+              <PlaceholderBox width="200px" height="200px" shade="#888" />
+            </div>
+
+            
+          </div>
+
+
+          {/* Buttons row */}
+          <div className="hero-buttons">
+            <PlaceholderBox width="150px" height="45px" shade="#222" />
+            <PlaceholderBox width="150px" height="45px" shade="#444" />
+          </div>
+        </div>
+
+      </div>
+    <div style={{display: "flex" , flexDirection: "column", justifyContent: "space-evenly"}}>
+      <PlaceholderBox width="300px" height="400px" borderRadius={30} />
       {/* Skills row */}
-      <div className="hero-skills">
-        <PlaceholderCircle size="40px" shade="#999" />
-        <PlaceholderCircle size="40px" shade="#777" />
-        <PlaceholderCircle size="40px" shade="#555" />
-        <PlaceholderCircle size="40px" shade="#333" />
-      </div>
-
-      {/* Buttons row */}
-      <div className="hero-buttons">
-        <PlaceholderBox width="150px" height="45px" shade="#222" />
-        <PlaceholderBox width="150px" height="45px" shade="#444" />
+            <div className="hero-skills">
+              <PlaceholderCircle size="40px" shade="#999" />
+              <PlaceholderCircle size="40px" shade="#777" />
+              <PlaceholderCircle size="40px" shade="#555" />
+              <PlaceholderCircle size="40px" shade="#333" />
+            </div>
       </div>
     </section>
   );
