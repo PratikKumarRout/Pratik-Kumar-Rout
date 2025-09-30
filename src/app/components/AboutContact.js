5024533db989576970b55fb785609e66abbd0d57
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-09-30 23:25:49 +0530

    change in about section ui version 0.2.5

--- src/app/components/AboutContact.js
+++ src/app/components/AboutContact.js
@@ -1,5 +1,6 @@
 import PlaceholderBox from "./PlaceholderBox";
 import PlaceholderCircle from "./PlaceholderCircle";
+import styles from "./AboutContact.module.css";
 
 export default function AboutContact() {
     return (
@@ -17,18 +18,23 @@ export default function AboutContact() {
                 backgroundColor: "#e0e4ea",
                 borderRadius: "20px"
             }}>
-                <div style={{ flex: 1 }}>
-                    <span>About</span>
-                    <p>I&apos;m a software developer specializing in scalable web and mobile apps with expertise in React, Next.js, Flutter, and Firebase.
+                <div style={{ flex: 1, placeItems: "center" }}>
+                    <h2>About</h2>
+                    <p className={styles.para}>I&apos;m a software developer specializing in scalable web and mobile apps with expertise in React, Next.js, Flutter, and Firebase.
                         I combine clean, minimal UI design with modular, future-ready architecture to deliver apps that are both elegant and high-performing.
-                        With experience mentoring students in web development and UI/UX, I bring clarity, precision, and creativity to every project I take on.</p>
+                        With experience in software development and UI/UX, I bring clarity, precision, and creativity to every project I take on.</p>
                 </div>
-                <div style={{ flex: 1 }}>
-                    <PlaceholderBox width={150} height={40} shade="#bbb" />
+                <div style={{ flex: 1, placeItems: "center" }}>
+                    <h2>Contact</h2>
                     <div style={{ display: "flex" }}>
-                        <PlaceholderCircle size={50} shade="var(--accent)" />
-                        <PlaceholderCircle size={50} shade="var(--accent)" />
-                        <PlaceholderCircle size={50} shade="var(--accent)" />
+                        <ul>
+                            <li>Mail</li>
+                            <li>Git hub</li>
+                            <li>Linkedin</li>
+                            <li>Insta</li>
+                            <li>WhatsApp</li>
+                            <li>Phone</li>
+                        </ul>
                     </div>
                 </div>
             </div>
