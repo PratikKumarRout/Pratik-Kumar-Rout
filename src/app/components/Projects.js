73376380f44a2b2f38ae53344eb15e64cd3d9529
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-10-04 11:44:14 +0530

    style: change project section responsive version 1.19.02

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,11 +1,19 @@
 // components/Projects.js
 import Link from "next/link";
 import styles from "./projects.module.css";
+import Image from "next/image";
 
 const projects = [
   {
     label: "Minimal Clock UI",
     url: "https://github.com/PratikKumarRout/neo-clock",
+    imgUrl: "/file.svg",
+    about: "A Flutter based fullscreen analog clock app with an aesthetics UI design. Functions as a wallpaper clock, combining minimal UI with smooth, modern aesthetics."
+  },
+  {
+    label: "Minimal Clock UI",
+    url: "https://github.com/PratikKumarRout/neo-clock",
+    imgUrl: "/file.svg",
     about: "A Flutter based fullscreen analog clock app with an aesthetics UI design. Functions as a wallpaper clock, combining minimal UI with smooth, modern aesthetics."
   },
 ];
@@ -18,8 +26,12 @@ export default function Projects() {
         <div className={styles.projects_container}>
           {projects.map((project, index) => (
             <div key={index} className={styles.project_chip}>
-              <div className={styles.project_image}>
-                 </div>
+              <Image
+                width={300}
+                height={300}
+                src={project.imgUrl}
+                alt="Project Image"
+              />
               <div className={styles.project_details}>
                 <h3>{project.label}</h3>
                 <p>{project.about}</p>
