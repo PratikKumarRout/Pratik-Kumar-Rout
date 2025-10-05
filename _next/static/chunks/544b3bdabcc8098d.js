60e821915b0ac69228616bbee548219542c787db
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-10-05 11:39:12 +0530

    deploy static export

---
+++ _next/static/chunks/544b3bdabcc8098d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/91a729da87c196ef.js",
+  "static/chunks/turbopack-eb162a07da3b8bd9.js"
+])
---
+++ _next/static/chunks/b6dbb42b689d4434.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/91a729da87c196ef.js",
+  "static/chunks/turbopack-9b7be51938115251.js"
+])
