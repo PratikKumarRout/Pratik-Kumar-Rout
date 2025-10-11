7c926309417105b7ce4cb7412f31bf896f780396
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-10-11 11:10:56 +0530

    add skill section

---
+++ src/app/components/Skills.js
@@ -0,0 +1,10 @@
+import styles from "./Skills.module.css";
+
+export default function Skills () {
+    return (
+        <section className={styles.skill_section}>
+            
+
+        </section>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <main role="main">
       <Hero />
       <Projects />
+      <Skills/>
       <Certificates />
       <AboutContact />
       </main>
