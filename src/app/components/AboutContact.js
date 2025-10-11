c656d03b0843995384cc39f5b3dfe3d213ddd6fb
Pratik Kumar Rout <routpratikiven@gmail.com>
2025-10-11 11:54:32 +0530

    new skill section

--- src/app/components/AboutContact.js
+++ src/app/components/AboutContact.js
@@ -24,7 +24,7 @@ export default function AboutContact() {
                     <p className={styles.para}>“Collaboration Starts with a Hello.” <br />
                         The best way to reach me is through email, but I&apos;m also available on other platforms.
                     </p>
-                    <div style={{ marginTop: "1rem" }} className={styles.list_link}>
+                    <div className={styles.list_link}>
                         {/* make a list of those contacts using map function */}
                         {contacts.map((contact, index) => (
                             <div key={index} className={styles.contact_list}>
--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -1,9 +1,25 @@
 import styles from "./Skills.module.css";
 
+const skills = [
+    {lable: "Programming Languages" , language: "Java, Dart, Python"},
+    {lable: "Web developement" , language: "React, HTML, CSS, JS"},
+    {lable: "Framework" , language: "NextJS, Flutter, Expo, NodeJS"},
+    {lable: "Database" , language: "MySQL, Firebase"},
+    {lable: "Tools" , language: "VS Code, AI tools, Postman"},
+    {lable: "Version Control", language: "Git, Git hub"}
+];
+
 export default function Skills () {
     return (
         <section className={styles.skill_section}>
-            
+            <h2>Skills</h2>
+            <div className={styles.skills_container}>
+                {skills.map((skill, index) => (
+                    <div key={index}> 
+                      <p>{skill.lable}: {skill.language}</p>
+                    </div>
+                ))}
+            </div>
 
         </section>
     );
